81e4c334e9688802d556bd94a744e37dbd9bc69d
taesoo <taesoo@aift.kr>
2023-03-08 10:21:24 +0900

    adsense 모듈 수정5

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import styles from './styles.module.css'
+
 declare global {
   interface Window {
     adsbygoogle?: { push: (p: object) => void };
@@ -15,14 +17,17 @@ export class GoogleAdSense extends React.Component {
 
   render() {
     return (
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client="ca-pub-1882367656929494"
-        data-ad-slot="6200588402"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
+      <div className={styles.googleAdsense}>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-1882367656929494"
+          data-ad-slot="6200588402"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      </div>
+
     );
   }
 }
