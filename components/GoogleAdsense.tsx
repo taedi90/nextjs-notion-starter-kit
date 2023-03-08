1a3ea67a1ca5fdccbf247b7aac4e9796c3188c21
taesoo <taesoo@aift.kr>
2023-03-08 10:52:17 +0900

    adsense 모듈 수정7

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -17,17 +17,14 @@ export class GoogleAdSense extends React.Component {
 
   render() {
     return (
-      <div className={styles.googleAdsense}>
-        <ins
-          className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-1882367656929494"
-          data-ad-slot="6200588402"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
-      </div>
-
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-1882367656929494"
+        data-ad-slot="6200588402"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
     );
   }
 }
