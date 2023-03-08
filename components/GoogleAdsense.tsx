5a7212da6277ec7f7bfeed2c2f5d0c4f8608fad4
taesoo <taesoo@aift.kr>
2023-03-08 10:03:15 +0900

    adsense 모듈 수정

--- components/GoogleAdsense.tsx
+++ components/GoogleAdsense.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
+import styles from './styles.module.css'
 export class GoogleAdSense extends React.Component {
-  // componentDidMount() {
-  //   // Google AdSense 코드를 여기에 삽입하세요
-  //   // 예시: (테스트 광고 코드)
-  //   (window.adsbygoogle = window.adsbygoogle || []).push({});
-  // }
+  componentDidMount() {
+    if (window.adsbygoogle) {
+      window.adsbygoogle.push({});
+    }
+  }
 
   render() {
     return (
-      <div>
-        {/* 광고를 노출할 영역 */}
+      <div className={styles.googleAdsense}>
         <ins
           className="adsbygoogle"
           style={{ display: 'block' }}
@@ -19,9 +19,6 @@ export class GoogleAdSense extends React.Component {
           data-ad-format="auto"
           data-full-width-responsive="true"
         />
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
       </div>
     );
   }
